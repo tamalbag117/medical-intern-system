018a900adce46a82f0287be1516327111f9b5408
tamalbag117 <tamalbag107@gmail.com>
2026-04-30 11:40:38 +0530

    Fixed Admin controller

--- src/main/java/com/mims/medicalinternsystem/controller/ActivityController.java
+++ src/main/java/com/mims/medicalinternsystem/controller/ActivityController.java
@@ -96,4 +96,6 @@ public class ActivityController {
     ) {
         return service.pendingPaged(page, size);
     }
+
+
 }
---
+++ src/main/java/com/mims/medicalinternsystem/controller/AdminController.java
@@ -0,0 +1,40 @@
+package com.mims.medicalinternsystem.controller;
+
+import com.mims.medicalinternsystem.entity.Role;
+
+import com.mims.medicalinternsystem.entity.User;
+import com.mims.medicalinternsystem.repository.UserRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/admin")
+public class AdminController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("/users")
+    public List<User> allUsers() {
+        return userRepository.findAll();
+    }
+
+    @PutMapping("/users/{id}/role")
+    public User updateRole(@PathVariable Long id, @RequestParam String role) {
+
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+
+        user.setRole(Role.valueOf(role.toUpperCase())); // ✅ FIX
+
+        return userRepository.save(user);
+    }
+
+    @DeleteMapping("/users/{id}")
+    public void deleteUser(@PathVariable Long id) {
+        userRepository.deleteById(id);
+    }
+}
